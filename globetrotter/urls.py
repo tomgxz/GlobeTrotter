c83bd7d05cdeb0497f9c4913bd601baa916bf7b1
Tomgxz <thomaslee10.TL@gmail.com>
2024-01-13 14:53:01 +0000

    robots.txt

--- globetrotter/urls.py
+++ globetrotter/urls.py
@@ -2,11 +2,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from django.views.static import serve 
+from django.views.generic.base import TemplateView
 
 from . import views
 from .localsettings import APP_NAME as app_name
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("get/locations/", views.getlocations, name="getlocations")
+    path("get/locations/", views.getlocations, name="getlocations"),
+    
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    )
 ]
